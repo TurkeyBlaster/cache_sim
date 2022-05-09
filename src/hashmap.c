2b5efb0083fe12f2ad836e937520781cc12713af
TurkeyBlaster <ananthmadan03@gmail.com>
2022-05-08 21:44:24 -0500

    First Iteration of Hashmap

--- src/hashmap.c
+++ src/hashmap.c
@@ -1,10 +1,12 @@
 #include "hashmap.h"
+#include <string.h>
 
-HashMap *hashmap_malloc(size_t capacity, size_t data_size)
+HashMap *hashmap_malloc(uint capacity, uint data_size)
 {
-    HashMap *map = (HashMap *)malloc(sizeof(char *) + 3 * sizeof(size_t));
+    HashMap *map = (HashMap *)malloc(sizeof(HashMap));
     map->map = malloc(capacity*data_size);
-    map->cell_size = 0;
+    map->cell_attrs = calloc(capacity, sizeof(CellAttrs));
+    map->cell_size = data_size;
     map->capacity = capacity;
     map->size = 0;
     return map;
@@ -16,7 +18,97 @@ void hashmap_free(HashMap *map)
     free(map);
 }
 
-int insert(HashMap *map, void *elem)
+bool insert(HashMap *map, void *elem, bool probe)
 {
+    if (map->size >= map->capacity)
+    {
+        return false;
+    }
     
+    uint hash = map->hash_algo((const) elem);
+    uint index = hash % map->capacity;
+    bool first_empty_set = false;
+    uint first_empty = index;
+    if (probe)
+    {
+        for (uint i = 0; i < map->capacity; ++i)
+        {
+            if (map->cell_attrs[index].flag)
+            {
+                if (map->cell_attrs[index].flag == 1 && !first_empty_set)
+                {
+                    first_empty = index;
+                }
+                if (map->cell_attrs[index].hash == hash)
+                {
+                    if (map->cell_attrs[index].flag == 1)
+                    {
+                        map->cell_attrs[index].flag = 2;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                ++index;
+                index %= map->capacity;
+            }
+            else
+            {
+                goto insert;
+            }
+        }
+        if (first_empty_set)
+        {
+            index = first_empty;
+            goto insert;
+        }
+        return false;
+    }
+insert:
+    memcpy((char *)map->map + map->cell_size * index, (char *)elem, map->cell_size);
+    map->cell_attrs[index].flag = 2;
+    map->cell_attrs[index].hash = hash;
+    ++map->size;
+    return true;
 }
+
+bool remove(HashMap *map, void *elem)
+{
+    if (map->size == 0)
+    {
+        return false;
+    }
+    uint hash = map->hash_algo((const) elem);
+    uint index = hash % map->capacity;
+    for (uint i = 0; i < map->capacity; ++i)
+    {
+        if (map->cell_attrs[index].flag)
+        {
+            if (map->cell_attrs[index].hash == hash)
+            {
+                if (map->cell_attrs[index].flag == 2)
+                {
+                    map->cell_attrs[index].flag = 1;
+                    --map->size;
+                    if (map->size == 0)
+                    {
+                        memset((char *)map->cell_attrs, 0, map->capacity*sizeof(CellAttrs));
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            ++index;
+            index %= map->capacity;
+        }
+    }
+    return false;
+}
+
+// uint find(HashMap *map, uint hash, bool *fail)
+// {}
--- src/hashmap.h
+++ src/hashmap.h
@@ -2,20 +2,38 @@
 #define HASHMAP_H
 
 #include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct CellAttrs
+{
+    uint hash;
+    char flag;
+} CellAttrs;
 
 typedef struct HashMap
 {
     void *map;
-    size_t cell_size;
-    size_t capacity;
-    size_t size;
+    CellAttrs *cell_attrs;
+    uint cell_size;
+    uint capacity;
+    uint size;
+    uint (*hash_algo)(const void *elem);
 } HashMap;
 
-HashMap *hashmap_malloc(size_t capacity, size_t data_size);
+HashMap *hashmap_malloc(uint capacity, uint data_size);
 void hashmap_free(HashMap *map);
-int insert(HashMap *map, void *elem);
-int remove(HashMap *map, void *elem);
-size_t find(HashMap *map, void *elem);
-int hash()
+bool insert(HashMap *map, void *elem, bool probe);
+bool remove(HashMap *map, void *elem);
+bool find(HashMap *map, uint hash, uint *index);
+
+// Taken from https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key
+// answer by Thomas Mueller
+uint hash_int(uint x)
+{
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return x;
+}
 
 #endif // HASHMAP_H
